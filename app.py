be0f9b4e55883844c1e3eb8209a70dcc25e75cc5
hordonezg <105169387+hordonezg@users.noreply.github.com>
2025-08-31 14:58:23 -0600

    Bind to $PORT en Render + normalizar DATABASE_URL

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")  # fallback local
 # Ajuste para SQLite (check_same_thread)
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
+
+# Normalizar prefijo postgres:// -> postgresql+psycopg2://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
+
+
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
